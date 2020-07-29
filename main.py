888ee6ed4d312ae4f912feae0bc3de3e8df88ea3
KalpasWang <48082968+KalpasWang@users.noreply.github.com>
2020-07-29 23:08:59 +0800

    add openpyxl

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os, ast, requests, json
+from openpyxl import load_workbook
 
 url = 'http://opendata.epa.gov.tw/webapi/Data/REWIQA/?$orderby=SiteName&$skip=0&$top=1000&format=json'
 html = requests.get(url).text#.encode('utf-8-sig')
 
 json_data = ast.literal_eval(html)
-print(json.dumps(json_data[0], indent=2))
+# print(json.dumps(json_data[0], indent=2))
+
+wb_name = 'pm25.xlsx'
+wb = load_workbook(wb_name)
+print(wb)
